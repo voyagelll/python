a6b1e257a566b0582c7c634ce6f4ec6303b3dbac
jiangzhaohui <jiang_zhaohui@outlook.com>
2019-12-02 18:21:44 +0800

    算法

--- single_link_cycle_list2.py
+++ single_link_cycle_list2.py
@@ -8,13 +8,12 @@ class Node:
 
 
 class SingleCycleLinkList(Linked_List):
-    # def __init__(self, head=None):
-    #     self.head = head
+
     pass
 
 
 if __name__ == '__main__':
     cll = SingleCycleLinkList()
     # print(help(cll))
-    cll.init_list([1,2,3,4])
-    cll.travel()
+    # cll.init_list([1,2,3,4])
+    # cll.travel()
