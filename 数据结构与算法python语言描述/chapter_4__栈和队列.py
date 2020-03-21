ee71decf6e79e30dd703127a117dbfe3ee42b9e7
jiangzhaohui <jiang_zhaohui@outlook.com>
2020-03-21 10:48:21 +0800

    update

--- 数据结构与算法python语言描述/chapter_4__栈和队列.py
+++ 数据结构与算法python语言描述/chapter_4__栈和队列.py
@@ -89,10 +89,11 @@ def check_parens(text):
 				return 
 			yield text[i], i 
 			i += 1
-
+	print([i for i in parentheses(text)])
 	st = SStack()
 
 	for pr, i in parentheses(text):
+		print(pr, i)
 		if pr in open_parens:
 			st.push(pr)
 		elif st.pop() != opposite[pr]:
@@ -101,4 +102,159 @@ def check_parens(text):
 	print("All paratheses are matched")
 	return True
 
-check_parens("{}]")
+# check_parens("{})aa")
+
+
+"""
+	后缀表达式
+"""
+
+# 增加一个求栈深度的方法
+class ESStack(SStack):
+	def depth(self):
+		return len(self.elem)
+
+
+def suf_exp_evaluator(exp):
+	operators = '+-*/'
+	st = ESStack()
+
+	for x in exp:
+		if x not in operators:
+			st.push(float(x))
+			continue
+		if st.depth() < 2:
+			raise SyntaxError('Short of operand(s)')
+		a = st.pop()
+		b = st.pop()
+
+		if x == '+':
+			c = a+b 
+		elif x == '-':
+			c = b-a
+		elif x == '*':
+			c = a*b
+		elif x=='/':
+			c = b/a 
+		else:
+			break 
+		st.push(c)
+	if st.depth() == 1:
+		return st.pop()
+	raise SyntaxError('Extra operand(s)')
+
+
+print(suf_exp_evaluator('3 5 - 6 17 4 * + * 3 /'.split(' ')))
+
+
+
+"""
+	背包问题
+"""
+
+
+
+
+
+
+"""
+	队列
+"""
+
+# 队列异常
+class QueueUnderflow(ValueError):
+	pass 
+
+
+class SQueue():
+	def __init__(self, init_len=8):
+		self._len = init_len 
+		self._elems = [0]*init_len 
+		self._head = 0 
+		self._num = 0 
+
+	def is_empty(self):
+		return self._num == 0 
+
+	def peek(self):
+		if self._num == 0:
+			raise QueueUnderflow()
+		return self._elems[self._head]
+
+	def dequeue(self):
+		if self._num == 0:
+			raise QueueUnderflow()
+		e = self._elems(self._head)
+		self._head = (self._head+1) % self._len 
+		self.num -= 1 
+		return e 
+
+	def enqueue(self, e):
+		if self._num == self._len:
+			self.__extend()
+		self._elems[(self._head+self._num) % self._len] = e 
+		self._num += 1 
+
+	def __extend(self):
+		old_len = self._len 
+		self._len *= 2 
+		new_elems = [0]*self._len 
+		for i in range(old_len):
+			new_elems[i] = self._elems[(self._head+i)%old_len]
+		self._elems, self_head = new_elems, 0 
+
+		
+
+
+"""
+	栈和队列的应用
+"""
+
+# 迷宫求解
+dirs = [(0,1), (1,0), (0,-1), (-1,0)]
+
+def mark(maze, pos):            # 已经到过的位置
+	maze[pos[0]][pos[1]] = 2
+
+def passable(maze, pos):        # 检查位置是否可行
+	return maze[pos[0]][pos[1]] == 0
+
+# 递归实现
+def find_path(maze, pos, end):
+	mark(maze, pos)
+	if pos == end:
+		print(pos, ned=" ")
+		return True 
+	for i in range(4):
+		nextp = pos[0]+dirs[i][0], pos[1]+dirs[i][1]
+		if passable(maze, nextp):
+			if find_path(maze, nextp, end):
+				print(pos, end=" ")
+				return True 
+	return False 
+
+
+
+# 回溯法（栈）
+def maze_solver(maze, start, end):
+	if start == end:
+		print(start)
+		return 
+	st = SStack()
+	mark(maze, start)
+	st.push((start, 0))
+	while not st.i_empty():
+		pos, nxt = st.pop()
+		for i in range(nxt, 4):
+			nextp = (pos[0] + dirs[i][0],
+					 pos[1] + dirs[i][1])
+		if nextp == end:
+			print_path(end, pos, st)
+			return 
+		if passable(maze, nextp):
+			st.push((pos, i+1))
+			mark(maze, nextp)
+			st.push((nextp, 0))
+			break 
+	print("No path, found")
+	
